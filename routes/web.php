5a095572a709155471d6067f29a2ea455e402358
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-01-07 16:17:55 +0800

    Update Router web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Routing untuk login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Routing untuk registrasi
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+
+// Routting untuk halaman dashboard
+Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
 Route::get('/', function () {
     return view('welcome');
 });
