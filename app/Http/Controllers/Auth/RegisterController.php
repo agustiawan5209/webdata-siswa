a4d0c38f321838cfc15d13cf9ef61bd9818358ff
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-01-07 16:12:38 +0800

    Buat Auth Login dan Register Untuk Pengguna

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    // Memanggil Akses Untuk Tamu
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function showRegistrationForm(){
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $user = $this->create($request->all());
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+
+    /**
+     * validator
+     * Validasi Data dari Pengguna
+     * @param  mixed $data
+     * @return void
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * create
+     * Buat Pengguna Baru Pada Model User
+     * @param  mixed $data
+     * @return void
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ env('APP_NAME', 'DataSiswa') }} - Login</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased">
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="shadow-lg sm:mx-auto sm:w-full sm:max-w-md px-5 py-2">
+            <div class="w-full">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Daftar Akun Baru
+                </h2>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="list-item bg-transparent">
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-400 underline text-sm">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+
+            <div class="mt-10 w-full">
+                <form class="space-y-6" action="{{ route('register') }}" method="POST">
+                    @csrf
+                    @method('POST')
+                    <div>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Nama</label>
+                        <div class="mt-2">
+                            <input id="name" name="name" type="text" autocomplete="name" required
+                                class="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Alamat
+                            Email</label>
+                        <div class="mt-2">
+                            <input id="email" name="email" type="email" autocomplete="email" required
+                                class="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password"
+                                class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+
+                        </div>
+                        <div class="mt-2">
+                            <input id="password" name="password" type="password" autocomplete="current-password"
+                                required
+                                class="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password_confirmation"
+                                class="block text-sm font-medium leading-6 text-gray-900">Konfirmasi Password</label>
+
+                        </div>
+                        <div class="mt-2">
+                            <input id="password_confirmation" name="password_confirmation" type="password"
+                                autocomplete="current-password_confirmation" required
+                                class="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                    <div class="text-sm">
+                        <a href="{{ route('login') }}" class="font-semibold text-indigo-600 hover:text-indigo-500">Sudah
+                            Punya Akun</a>
+                    </div>
+                    <div>
+                        <button type="submit"
+                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Masuk</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
